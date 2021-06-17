267281ce3c5868142bae647b0e08de988019bcc5
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-17 13:54:31 -0600

    Add routes to controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :parks, only: :index
+      resources :forecast, only: :index
+      resources :campground, only: :index
     end
   end
 end
