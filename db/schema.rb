01e38ae0182bf63d6ec0cccd2b2705bd7e3cda96
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-17 14:31:51 -0600

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_200158) do
+ActiveRecord::Schema.define(version: 2021_06_17_202332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_06_17_200158) do
   create_table "searches", force: :cascade do |t|
     t.string "state_code"
     t.json "park_data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "api_key"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
