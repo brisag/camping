682c89b0ccf0100c08bea7dd740034895986b9ba
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-17 14:02:47 -0600

    Add search model

--- app/facades/national_parks_facade.rb
+++ app/facades/national_parks_facade.rb
@@ -1,9 +1,16 @@
 class NationalParksFacade
   class << self
     def get_parks(state)
-      results = NationalParksService.state_search(state)
-      results[:data].map do |park|
-        Park.new(park)
+      state_data = Search.find_by(state_code: state)
+      if state_data.nil?
+        results = NationalParksService.state_search(state)
+        state_data = Search.new(state_code: state, parks_data: parks[:data])
+        state_data.save
+      end
+      state_data.parks_data.map do |park|
+        Park.new(park.symbolize_keys)
+      # results[:data].map do |park|
+      #   Park.new(park)
       end
     end
 
---
+++ spec/factories/searches.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :search do
+    state_code { "MyString" }
+    park_data { "" }
+  end
+end
