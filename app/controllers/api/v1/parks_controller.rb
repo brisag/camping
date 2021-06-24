41ff1dcde44aad2ac3306039fc0ed5eefc01266a
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-24 08:37:10 -0600

    Add missing end

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -4,5 +4,6 @@ class Api::V1::ParksController < ApplicationController
       render json: ParkSerializer.new(parks)
     else
       render json: { error: "Please enter valid state code" }, status: :bad_request
+    end
   end
 end
