33731ad0907bb806cf4a17f5edef3f28461221d2
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-24 08:34:50 -0600

    Remove commented out code

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ParksController < ApplicationController
   def index
-    parks = NationalParksFacade.get_parks(params[:stateCode])
-    render json: ParkSerializer.new(parks)
+    if parks = NationalParksFacade.get_parks(params[:stateCode])
+      render json: ParkSerializer.new(parks)
+    else
+      render json: { error: "Please enter valid state code" }, status: :bad_request
   end
 end
