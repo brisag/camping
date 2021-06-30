4f61aa852efbfeebd0766506360b1efb461577fc
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-30 12:35:56 -0600

    Add validatiosn for search state code

--- app/controllers/api/v1/parks_controller.rb
+++ app/controllers/api/v1/parks_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::ParksController < ApplicationController
   def index
-    if parks = NationalParksFacade.get_parks(params[:stateCode])
+    # binding.pry
+    search = Search.new(state_code: params[:stateCode])
+    unless search.valid?
+      render json: { error: search.errors.full_messages }, status: :bad_request
+      return
+    end
+    if parks = NationalParksFacade.get_parks(search.state_code)
       render json: ParkSerializer.new(parks)
-    else
-      render json: { error: "Please enter valid state code" }, status: :bad_request
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
 
   def render_invalid_record(exception)
     render json: { error: exception.message }, status: :bad_request
+  end
+
+  def render_not_found_response
+    render json: { message: "Not found", code: "not_found" }, status: :not_found
   end
 end
